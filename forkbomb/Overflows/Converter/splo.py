9db4c040e88d9dbc5ca3acd51c76947136278df6
vl1856 <78276416+vl1856@users.noreply.github.com>
2024-11-15 21:15:19 +0300

    Update splo.py

--- forkbomb/Overflows/Converter/splo.py
+++ forkbomb/Overflows/Converter/splo.py
@@ -9,10 +9,10 @@ while True:
     print(p.recvline())
     print(p.recvline())
     print(p.recvline())
-    str = p.recvline().decode("utf-8")
+    last_str = p.recvline().decode("utf-8")
 
 
-    if str == "|_| |_| \\___|/_/\\_\\\r\n":  
+    if last_str == "|_| |_| \\___|/_/\\_\\\r\n":  
         res = str_with_number.decode("utf-8").split(" ")
         hex_str = hex(int(res[1]))[2:]
         payload = bytes(hex_str , 'utf-8')
@@ -20,7 +20,7 @@ while True:
         print(p.recvline())
         print(p.recvline())
 
-    elif str == " \\___/  \\___| \\__| \\__,_||_|\r\n":  
+    elif last_str == " \\___/  \\___| \\__| \\__,_||_|\r\n":  
         res = str_with_number.decode("utf-8").split(" ")
         oct_str = oct(int(res[1]))[2:]
         payload = bytes(oct_str , 'utf-8')
@@ -28,7 +28,7 @@ while True:
         print(p.recvline())
         print(p.recvline())
 
-    elif str == "|____/ |_| \\__, | |_____||_| |_| \\__,_||_| \\__,_||_| |_| \\___/    |_|  \r\n": 
+    elif last_str == "|____/ |_| \\__, | |_____||_| |_| \\__,_||_| \\__,_||_| |_| \\___/    |_|  \r\n": 
         res = str_with_number.decode("utf-8").split(" ")
         num = int(res[1])
         p.sendline(p64(num, endian='big'))
@@ -36,18 +36,9 @@ while True:
         print(p.recvline())
 
 
-    elif str == "|_____||_| \\__| \\__||_| \\___| |_____||_| |_| \\__,_||_| \\__,_||_| |_| \\___/    |_|  \r\n": 
+    elif last_str == "|_____||_| \\__| \\__||_| \\___| |_____||_| |_| \\__,_||_| \\__,_||_| |_| \\___/    |_|  \r\n": 
         res = str_with_number.decode("utf-8").split(" ")
         num = int(res[1])
         p.sendline(p64(num))
         print(p.recvline())
         print(p.recvline())
-
-
-
-
-
-
-
-
-
